5f4c613802c7f77dc39bf27112d296bf2e2c20cb
caleb85 <11528146+caleb85@users.noreply.github.com>
2020-02-27 19:27:00 -0500

    updated script documentation

--- microcenter-openbox-amd-motherboard.sh
+++ microcenter-openbox-amd-motherboard.sh
@@ -2,7 +2,7 @@
 # requires sed and html2text
 # this line pulls the data. Store 71 is sharonville, OH. Currently set to AMD openbox motherboards
 curl --cookie "storeSelected=071"  "https://www.microcenter.com/search/search_results.aspx?Ntk=all&sortby=match&prt=clearance&N=4294966996+4294818892&myStore=true" | html2text | sed -n '/\*\*\*\*\* Local Store or Chain Wide Options \*\*\*\*\*/,$p' | sed -n '/\*\*\*\*\* Product Results Pagination \*\*\*\*\*/q;p' | sed 's/ //g' > current.md
-#if current.md is blank then echo 0 in stock to the file as well a a random first line.
+#if current.md is blank then echo 0 in stock to current.md
 test -s current.md
 fs=$?
 if [ $fs = 1 ]; then
@@ -19,9 +19,10 @@ if [ fo = 1]; then
 	mv current.md old.md
 	exit
 fi
+#these two lines create the current data and the old data variables, and organize the data so it is only numbers
 cur=$(cat current.md | sed 's/[^0-9]*//g')
 old=$(cat old.md | sed 's/[^0-9]*//g')
-
+#this if statement compares the numbers and acts accordingly
 if [ cur > old ]; then
 	echo "current data is larger than the old data a new item is in stock pushbullet notification to phone needed"
 elif [ cur < old ]; then
@@ -31,6 +32,7 @@ elif [ cur = old ]; then
 else
 	echo "If you see this your are going to have a bad time"
 fi
+#replace the current data with the old data for later
 mv current.md old.md
 rm current.md
 exit
